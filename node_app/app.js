2364d01d7c4e499c6f083759bb2e3b03ddafc938
Filipe Monteiro <filipe@tfsolutions.com.br>
2014-05-23 16:00:43 -0300

    Adicionando tratamento de mensagem de todos os usuários

--- node_app/app.js
+++ node_app/app.js
@@ -1,15 +1,28 @@
 var io = require('socket.io').listen(3090),
 	redis = require('redis'),
-	clientRedis = redis.createClient();
+	clientRedis = redis.createClient(),
+	clientRedisPublish = redis.createClient();
 
 clientRedis.subscribe("new_user");
+clientRedis.subscribe("users_loaded");
 
 io.sockets.on("connection", function(socket) {
-	
+	socket.on("get_users", function(data) {
+		console.log(data);
+		console.log(socket.id);
+		var message = {socket: socket.id};
+		clientRedisPublish.publish("get_users", JSON.stringify(message));
+	});
 });
 
 clientRedis.on("message", function(channel, message) {
 	console.log("Channel: " + channel);
 	console.log(message);
-	io.sockets.emit(channel, message);
+	if (channel == "users_loaded") {
+		message = JSON.parse(message);
+		socket = io.sockets.sockets[message.socket];
+		socket.emit(channel, message);
+	} else {
+		io.sockets.emit(channel, message);
+	}	
 });
--- rails_app/app/assets/javascripts/guruceApp.js
+++ rails_app/app/assets/javascripts/guruceApp.js
@@ -6,12 +6,18 @@ guruceApp.controller("UsersController", ['$scope', function($scope) {
 
 	$scope.init = function() {
 		$scope.socket = io.connect("http://localhost:3090");
+		$scope.socket.emit("get_users", "get all data");
 		$scope.addListeners();
 	};
 
 	$scope.addListeners = function() {
 		$scope.socket.on("new_user", function(data) {
 			$scope.users.push(JSON.parse(data));
+			$scope.$digest();
+		});
+
+		$scope.socket.on("users_loaded", function(data) {
+			$scope.users = data.users;
 			$scope.$digest();
 		});
 	};
